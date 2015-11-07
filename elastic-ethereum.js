69b2d65d4fedbea51ff9191c25b5b2185f6652f7
Jonathan Brown <jbrown@bluedroplet.com>
2015-11-08 00:30:11 +0700

    Connect to Ethereum.

--- elastic-ethereum.js
+++ elastic-ethereum.js
@@ -2,3 +2,5 @@ var Web3 = require('web3');
 var web3 = new Web3();
 var elasticsearch = require('elasticsearch');
 var config = require('config');
+
+web3.setProvider(new web3.providers.HttpProvider(config.get('ethereum.provider')));
