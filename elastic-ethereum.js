fdb73eaba94ed348f55f3c9e6ea4c3f94d6c0b28
Jonathan Brown <jbrown@bluedroplet.com>
2015-11-08 00:33:22 +0700

    Connect to Elasticsearch.

--- elastic-ethereum.js
+++ elastic-ethereum.js
@@ -4,3 +4,7 @@ var elasticsearch = require('elasticsearch');
 var config = require('config');
 
 web3.setProvider(new web3.providers.HttpProvider(config.get('ethereum.provider')));
+
+var client = new elasticsearch.Client({
+  host: config.get('elasticsearch.host')
+});
